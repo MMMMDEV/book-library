2fb452a50cd087ed0c61f2d27ff53e1315586fa0
mario <manriqueduran123@gmail.com>
2022-09-14 13:31:55 -0500

    added a working modal to obtain information for card

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+// section to produce a arr of the book info
 let myLibrary = [];
 
 function book(title, author, pages, read) {
@@ -20,11 +21,40 @@ function displayBook() {
     }
 }
 
-
+// getting information from form
+const cardBtn = document.querySelector(".card-btn");
+const overlayer = document.querySelector(".overlayer");
+const modal = document.querySelector(".modal");
+const form = document.querySelector("form");
+const input1 = document.querySelectorAll(".txt1"); 
+const input2 = document.querySelectorAll(".txt2");
+const input3 = document.querySelectorAll(".txt3");
+const input4 = document.querySelectorAll(".checkbox");
+const formSubmit = document.querySelector(".form-submit");
 
 const Flash = new book("Flash", "Barry Alen", "76", "read");
-const Doctor = new book("Doctor", "Ally Best", "192", "not read");
-
+const Doctor = new book("Doctor", "Ally Best", "192", "not read"); 
 addBookToLibrary(Flash.info());
 addBookToLibrary(Doctor.info());
-displayBook();
+
+formSubmit.addEventListener("click", e => {
+    if (input1[0].value !== "" && input2[0].value !== "" && input3[0].value !== "") {
+        let otherBook = new book( input1[0].value, input2[0].value, input3[0].value, input4[0].value);
+        addBookToLibrary(otherBook.info());
+        displayBook();
+    }
+});
+
+cardBtn.addEventListener("click", e => {
+    modal.classList.add("modal-active");
+    overlayer.classList.add("active");
+})
+
+overlayer.addEventListener("click", e => {
+    modal.classList.remove("modal-active");
+    overlayer.classList.remove("active");
+})
+
+
+
+
