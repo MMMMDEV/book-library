08b6a5cae41cafba3f99d7a7230e0e0e31edabde
mario <manriqueduran123@gmail.com>
2022-09-14 19:43:54 -0500

    added card and how it works, still working on how to function delete btn

--- javascript.js
+++ javascript.js
@@ -5,10 +5,7 @@ function book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
-    this.info = function() {
-        return "The " + this.title + " by " + this.author + ", " + this.pages + " pages, " + this.read;
-    }   
+    this.read = read;  
 };
 
 function addBookToLibrary(book) {
@@ -22,6 +19,7 @@ function displayBook() {
 }
 
 // getting information from form
+const cardContent = document.querySelector(".card-content");
 const cardBtn = document.querySelector(".card-btn");
 const overlayer = document.querySelector(".overlayer");
 const modal = document.querySelector(".modal");
@@ -31,23 +29,81 @@ const input2 = document.querySelectorAll(".txt2");
 const input3 = document.querySelectorAll(".txt3");
 const input4 = document.querySelectorAll(".checkbox");
 const formSubmit = document.querySelector(".form-submit");
+let card;
+let removeBtn;
 
-const Flash = new book("Flash", "Barry Alen", "76", "read");
-const Doctor = new book("Doctor", "Ally Best", "192", "not read"); 
-addBookToLibrary(Flash.info());
-addBookToLibrary(Doctor.info());
+function createCard() {
+    card = document.createElement("div");
+    card.classList.add("card");
 
-formSubmit.addEventListener("click", e => {
+    const cardTop = document.createElement("div");
+    cardTop.classList.add("card-top");
+
+    const titleTxt = document.createElement("p");
+    titleTxt.classList.add("title-txt");
+
+    const authorTxt = document.createElement("p");
+    authorTxt.classList.add("author-txt");
+
+    const pagesNum = document.createElement("p");
+    pagesNum.classList.add("pages-num");
+
+    const readToggle = document.createElement("input");
+    readToggle.classList.add("read-toggle");
+    readToggle.type = "checkbox";
+
+    removeBtn = document.createElement("button");
+    removeBtn.classList.add("remove-btn");
+
+    const cardBottom = document.createElement("div");
+    cardBottom.classList.add("card-bottom");
+
+    card.insertAdjacentElement("beforeend", cardTop);
+    cardTop.insertAdjacentElement("beforeend", titleTxt);
+    cardTop.insertAdjacentElement("beforeend", authorTxt);
+    cardTop.insertAdjacentElement("beforeend", pagesNum);
+    card.insertAdjacentElement("beforeend", cardBottom);
+    cardBottom.insertAdjacentElement("beforeend", readToggle);
+    cardBottom.insertAdjacentElement("beforeend", removeBtn);
+    cardContent.insertAdjacentElement("beforeend", card);
+
+    titleTxt.textContent = input1[0].value;
+    authorTxt.textContent = input2[0].value;
+    pagesNum.textContent = input3[0].value;
+    removeBtn.textContent = "Delete";
+    if (input4[0].checked == true) {
+        readToggle.checked = true;
+    } else {
+        readToggle.checked = false;
+    }
+}
+
+function createBook() {
     if (input1[0].value !== "" && input2[0].value !== "" && input3[0].value !== "") {
         let otherBook = new book( input1[0].value, input2[0].value, input3[0].value, input4[0].value);
-        addBookToLibrary(otherBook.info());
-        displayBook();
+        createCard();
+        modal.classList.remove("modal-active");
+        overlayer.classList.remove("active");
+        input1[0].value = "";
+        input2[0].value = "";
+        input3[0].value = "";
+        input4[0].checked = false;
     }
+}
+
+function removeCard() {
+    card.remove();
+}
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    createBook();
 });
 
 cardBtn.addEventListener("click", e => {
     modal.classList.add("modal-active");
     overlayer.classList.add("active");
+    console.log(myLibrary);
 })
 
 overlayer.addEventListener("click", e => {
@@ -55,6 +111,8 @@ overlayer.addEventListener("click", e => {
     overlayer.classList.remove("active");
 })
 
-
+removeBtn.addEventListener("click", e => {
+    removeCard();
+});
 
 
